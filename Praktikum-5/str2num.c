da1e44b7c18267fd94b8e6a628ee9110f56e8d95
DanielDPW <150114531+DanielDPW@users.noreply.github.com>
2024-10-19 18:01:35 +0700

    Added PraPraktikum-5

---
+++ Praktikum-5/str2num.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <string.h>
+#include "mesinkata.h"
+
+boolean compare(Word x, char y[]) {
+    if (x.Length != strlen(y)) {
+        return false;
+    }
+    for (int i = 0; i < x.Length; i++) {
+        if (x.TabWord[i] != y[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+boolean checkEndNumPunct(Word x, char * punct) {
+    boolean havePunct = false;
+    if (x.TabWord[x.Length - 1] == '.') {
+        *punct = x.TabWord[x.Length - 1];
+        havePunct = true;
+    }
+    return havePunct;
+}
+
+int main(void) {
+    STARTWORD();
+
+    int num = 0;
+    char punct = '\0';
+    boolean inNum = false;
+    Word current;
+
+    while (!EndWord) {
+        strcpy(current.TabWord, currentWord.TabWord);
+        current.Length = currentWord.Length;
+        punct = '\0';
+        if (checkEndNumPunct(current, &punct)) {
+            currentWord.Length--;
+        }
+        if (compare(current, "nol")) {
+            if (!inNum) {
+                inNum = true;
+            }
+        } else if (compare(current, "satu")) {
+            if (!inNum) {
+                inNum = true;
+            }
+            num += 1;
+        } else if (compare(current, "dua")) {
+            if (!inNum) {
+                inNum = true;
+            }
+            num += 2;
+        } else if (compare(current, "tiga")) {
+            if (!inNum) {
+                inNum = true;
+            }
+            num += 3;
+        } else if (compare(current, "empat")) {
+            if (!inNum) {
+                inNum = true;
+            }
+            num += 4;
+        } else if (compare(current, "lima")) {
+            if (!inNum) {
+                inNum = true;
+            }
+            num += 5;
+        } else if (compare(current, "enam")) {
+            if (!inNum) {
+                inNum = true;
+            }
+            num += 6;
+        } else if (compare(current, "tujuh")) {
+            if (!inNum) {
+                inNum = true;
+            }
+            num += 7;
+        } else if (compare(current, "delapan")) {
+            if (!inNum) {
+                inNum = true;
+            }
+            num += 8;
+        } else if (compare(current, "sembilan")) {
+            if (!inNum) {
+                inNum = true;
+            }
+            num += 9;
+        } else if (compare(current, "sepuluh") || compare(current, "belas")) {
+            if (!inNum) {
+                inNum = true;
+            }
+            num += 10;
+        } else if (compare(current, "sebelas")) {
+            if (!inNum) {
+                inNum = true;
+            }
+            num += 11;
+        } else if (compare(current, "puluh")) {
+            if (!inNum) {
+                inNum = true;
+            }
+            num += 10 * (num % 10) - (num % 10);
+        } else if (compare(current, "seratus")) {
+            if (!inNum) {
+                inNum = true;
+            }
+            num += 100;
+        } else if (compare(current, "ratus")) {
+            if (!inNum) {
+                inNum = true;
+            }
+            num += 100 * (num % 100) - (num % 100);
+        } else {
+            if (inNum) {
+                printf("%d ", num);
+                num = 0;
+                inNum = false;
+            }
+        }
+
+        ADVWORD();
+        if (!inNum) {
+            int len = current.Length;
+            for (int i = 0; i < len; i++) {
+                printf("%c", current.TabWord[i]);
+            }
+            if (!EndWord) {
+                printf(" ");
+            }
+        }
+        
+    }
+    if (inNum) {
+        printf("%d", num);
+        num = 0;
+        inNum = false;
+    }
+    
+    printf("\n");
+    return 0;
+}
